1ab47ff3428ce2010dbd5e2f8efe10c1478b45a7
John Deen Bucog <JDBucog@yahoo.com>
2022-07-08 01:55:39 -0700

    timecard styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import UserInfo from "./components/UserInfo"
 import TimeCard from "./components/TimeCard"
-import "./App.css"
+import "./styles/app.css"
 import jsondata from "./utils/data.json"
 
 function App() {
-  return <div className="App">
-    
-  </div>
+  return (
+    <div className="App">
+      {jsondata.map((data, index) => {
+        return <TimeCard props={data} />
+      })}
+    </div>
+  )
 }
 
 export default App
--- src/components/TimeCard.jsx
+++ src/components/TimeCard.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from "react"
 
-const TimeCard = () => {
+// {
+//   "title": "Work",
+//   "timeframes": {
+//     "daily": {
+//       "current": 5,
+//       "previous": 7
+//     },
+//     "weekly": {
+//       "current": 32,
+//       "previous": 36
+//     },
+//     "monthly": {
+//       "current": 103,
+//       "previous": 128
+//     }
+//   }
+// },
+
+const TimeCard = ({ props }) => {
+  const { title, timeframes, daily, weekly, monthly } = props
   return (
-    <div>TimeCard</div>
+    <div className="timeCard">
+      <div className="timeCard__inner-container">
+        <h2 className="timeCard__name">{title}</h2>
+        <p className="timeCard__hours">{timeframes.weekly.current} Hrs</p>
+        <p className="timeCard__previous-week">
+          Last Week - {timeframes.weekly.previous} Hrs
+        </p>
+      </div>
+    </div>
   )
 }
 
